de699d855ad5cb006c4a02d10d991ab350288f80
akaki <akaki@domain.com>
2023-10-11 17:47:46 +0300

    add theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Montserrat', 'sans-serif']
+    },
+    colors: {
+      main: "#00333E",
+      success: "#1B876F",
+      "bg-main": "#FFFCFA",
+      error: "#B22222",
+      white: '#fff',
+    },
   },
   plugins: [],
 }
